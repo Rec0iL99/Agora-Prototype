3f466ba615bb9aac1735d2b0d7c28a25a50893f8
Joel Mathew <joelmathewkoshy@karunya.edu>
2020-03-23 17:26:09 +0530

    Initial commit

---
+++ app/src/main/java/com/joel/agoraprototype/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.joel.agoraprototype
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 4000)
+
+    }
+}
