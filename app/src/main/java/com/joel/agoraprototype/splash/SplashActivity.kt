84aff01ad589b72b70c053feb900197962e382b7
Joel Mathew <joelmathewkoshy@karunya.edu>
2020-03-23 19:58:53 +0530

    added splash activity

--- app/src/main/java/com/joel/agoraprototype/splash/SplashActivity.kt
+++ app/src/main/java/com/joel/agoraprototype/splash/SplashActivity.kt
@@ -1,14 +1,21 @@
-package com.joel.agoraprototype
+package com.joel.agoraprototype.splash
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.ActionBar
+import com.joel.agoraprototype.MainActivity
+import com.joel.agoraprototype.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //hiding the appbar
+        val actionBar:ActionBar? = supportActionBar
+        actionBar?.hide()
 
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
