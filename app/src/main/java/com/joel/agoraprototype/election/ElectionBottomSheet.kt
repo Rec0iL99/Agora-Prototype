aa2da8862c7d6b7cc74c1ad3f71db85f22a334e4
Joel Mathew <joelmathewkoshy@karunya.edu>
2020-04-09 01:34:01 +0530

    added function for bottomsheet

--- app/src/main/java/com/joel/agoraprototype/election/ElectionBottomSheet.kt
+++ app/src/main/java/com/joel/agoraprototype/election/ElectionBottomSheet.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.joel.agoraprototype.R
+import kotlinx.android.synthetic.main.election_bottom_sheet.view.*
 
 class ElectionBottomSheet : BottomSheetDialogFragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +18,26 @@ class ElectionBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view: View = inflater.inflate(R.layout.election_bottom_sheet, container, false)
+
+        view.bt_generate_election_link.setOnClickListener {
+            Toast.makeText(activity, "Link copied to clipboard", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        view.bt_invite_voters.setOnClickListener {
+            Toast.makeText(activity, "Voter invite class", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        view.bt_voters.setOnClickListener {
+            Toast.makeText(activity, "Voters will be displayed", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        view.bt_delete_election.setOnClickListener {
+            Toast.makeText(activity, "Election has been deleted", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
         return view
     }
 
